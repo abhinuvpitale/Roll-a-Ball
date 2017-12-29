d3d253aded793f7ad36b6bf10246e35444aca4dc
abhinuvpitale <abhinuvpitale@gmail.com>
2017-12-29 08:04:52 -0500

    added Scene, Player and camera motion scripts.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+    // Late Update function makes sure that it is run after all Update functions have been run.
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
